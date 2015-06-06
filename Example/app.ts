915fcd2680a7826b46ce1f8419cefa0b79c2ecde
swarmttied <the-real-gio@outlook.com>
2015-06-06 11:19:24 -0700

    Before transitioning to Express

---
+++ Example/app.ts
@@ -0,0 +1,17 @@
+﻿/// <reference path="scripts/typings/node/node.d.ts"/>
+
+import http = require('http');
+
+var portNumber = 8087;
+
+function requestListener(request: http.ServerRequest, response: http.ServerResponse) {
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
+    response.write('Method: ' + request.method + '\n');
+    response.write('Url: ' + request.url + '\n');
+    response.write('Response Text Here');
+    response.end();
+}
+
+http.createServer(requestListener).listen(portNumber);
+
+console.log('Listening on localhost:' + portNumber);
